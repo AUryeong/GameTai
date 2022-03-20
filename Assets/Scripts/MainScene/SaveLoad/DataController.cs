88e0ef4998fa8524bb7a798c30c189669fd63862
AUryeong <jykee7030@gmail.coma>
2022-03-21 03:21:21 +0900

    주누 코퍼레이션

---
+++ Assets/Scripts/MainScene/SaveLoad/DataController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+public class DataController : MonoBehaviour
+{
+    static DataController _instance; 
+    public static DataController Instance 
+    { 
+        get 
+        { 
+            if (!_instance) 
+            { 
+                GameObject obj = new GameObject("DataController");
+                _instance = obj.AddComponent(typeof(DataController)) as DataController; 
+                DontDestroyOnLoad(obj);
+            } 
+            return _instance; 
+        } 
+    } 
+    
+    public string GameDataFileName = "JCSaveFile.json"; 
+    
+    public GameData _gameData; 
+    public GameData gameData 
+    {
+        get 
+        { 
+            if(_gameData == null)
+            { 
+                LoadGameData(); 
+                SaveGameData();
+            }
+            return _gameData;
+        }
+    } 
+    
+    private void Start() 
+    {
+        LoadGameData(); 
+        SaveGameData();
+    } 
+    
+    public void LoadGameData() 
+    {
+        string filePath = Application.persistentDataPath + GameDataFileName;
+        
+        if (File.Exists(filePath))
+        { 
+            string FromJsonData = File.ReadAllText(filePath); 
+            _gameData = JsonUtility.FromJson<GameData>(FromJsonData);
+        }
+        else 
+        {            
+            _gameData = new GameData();
+        } 
+    } 
+    
+    public void SaveGameData() 
+    {
+        File.WriteAllText(Application.persistentDataPath + GameDataFileName, JsonUtility.ToJson(gameData));
+    } 
+    
+    private void OnApplicationQuit()
+    { 
+        SaveGameData(); 
+    } 
+} 
---
+++ Assets/Scripts/MainScene/TitleButton/TitleButton.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine;
+
+public class TitleButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject blackground;
+
+    [SerializeField]
+    private GameObject newgamewindow;
+
+    [SerializeField]
+    private GameObject continuewindow;
+
+    [SerializeField]
+    private GameObject gameoutwindow;
+
+    [SerializeField]
+    private GameObject newgame;
+
+    [SerializeField]
+    private GameObject gameout;
+
+    private bool noclick = false;
+
+    private void Awake()
+    {
+        TextMeshProUGUI tmpro = this.gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        if (DataController.Instance.gameData.cleartutorial)
+        {
+            this.gameObject.GetComponent<Button>().interactable = true;
+            tmpro.color = new Color(tmpro.color.r, tmpro.color.g, tmpro.color.b, 1);
+        }
+        else
+        {
+            this.gameObject.GetComponent<Button>().interactable = false;
+            tmpro.color = new Color(tmpro.color.r, tmpro.color.g, tmpro.color.b, 0.35f);
+        }
+    }
+
+    IEnumerator WindowOff(GameObject obj, GameObject mokpyo)
+    {
+        if (!noclick)
+        {
+            blackground.gameObject.SetActive(false);
+            obj.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+            obj.GetComponent<RectTransform>().localPosition = Vector3.zero;
+            obj.SetActive(true);
+            noclick = true;
+            while (obj.GetComponent<RectTransform>().localScale.x > 0.02f)
+            {
+                obj.GetComponent<RectTransform>().localScale = Vector3.Lerp(obj.GetComponent<RectTransform>().localScale, Vector3.zero, Time.deltaTime * 20);
+                obj.GetComponent<RectTransform>().localPosition = Vector3.Lerp(obj.GetComponent<RectTransform>().localPosition, mokpyo.GetComponent<RectTransform>().localPosition, Time.deltaTime * 20);
+                yield return new WaitForFixedUpdate();
+            }
+            obj.GetComponent<RectTransform>().localScale = Vector3.zero;
+            obj.GetComponent<RectTransform>().localPosition = obj.GetComponent<RectTransform>().localPosition;
+            noclick = false;
+        }
+    }
+
+    IEnumerator WindowOn(GameObject obj, GameObject mokpyo)
+    {
+        if (!noclick)
+        {
+            blackground.gameObject.SetActive(true);
+            obj.GetComponent<RectTransform>().localScale = new Vector3(0, 0, 0);
+            obj.SetActive(true);
+            obj.GetComponent<RectTransform>().localPosition = mokpyo.GetComponent<RectTransform>().localPosition;
+            noclick = true;
+            while (obj.GetComponent<RectTransform>().localScale.x < 0.98f)
+            {
+                obj.GetComponent<RectTransform>().localScale = Vector3.Lerp(obj.GetComponent<RectTransform>().localScale, Vector3.one, Time.deltaTime * 20);
+                obj.GetComponent<RectTransform>().localPosition = Vector3.Lerp(obj.GetComponent<RectTransform>().localPosition, Vector3.zero, Time.deltaTime * 20);
+                yield return new WaitForFixedUpdate();
+            }
+            obj.GetComponent<RectTransform>().localScale = Vector3.one;
+            obj.GetComponent<RectTransform>().localPosition = Vector3.zero;
+            noclick = false;
+        }
+    }
+    public void NewGameNo()
+    {
+        StartCoroutine(WindowOff(newgamewindow, newgame));
+    }
+    public void ContinueNo()
+    {
+        StartCoroutine(WindowOff(continuewindow, gameObject));
+    }
+    public void GameOutNo()
+    {
+        StartCoroutine(WindowOff(gameoutwindow, gameout));
+    }
+    public void NewGame()
+    {
+        if (DataController.Instance.gameData.cleartutorial)
+        {
+            StartCoroutine(WindowOn(newgamewindow, newgame));
+        }
+        else if (!noclick)
+        {
+            DataController.Instance.SaveGameData();
+        }
+    }
+    public void Continue()
+    {
+        StartCoroutine(WindowOn(continuewindow, gameObject));
+    }
+
+    public void GameOut()
+    {
+        StartCoroutine(WindowOn(gameoutwindow, gameout));
+    }
+
+    
+
+    public void NewGameYes()
+    {
+        DataController.Instance._gameData = new GameData();
+        DataController.Instance.SaveGameData();
+        DataController.Instance.LoadGameData();
+    }
+    public void ContinueYes()
+    {
+        DataController.Instance.LoadGameData();
+    }
+    public void GameOutYes()
+    {
+        DataController.Instance.SaveGameData();
+        Application.Quit();
+    }
+}
