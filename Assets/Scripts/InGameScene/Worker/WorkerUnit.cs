4b081181ffdfad6515557812ee8a0c5681ccddca
AUryeong <jykee7030@gmail.coma>
2022-03-23 03:16:33 +0900

    워커 변경

--- Assets/Scripts/InGameScene/Worker/WorkerUnit.cs
+++ Assets/Scripts/InGameScene/Worker/WorkerUnit.cs
@@ -1,6 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+public enum Rank
+{
+    D,
+    C,
+    B,
+    A,
+    AA,
+    AAA
+}
+
+[Serializable]
+public class Worker
+{
+    public int health;
+    public int mental;
+    public int art;
+    public int programing;
+    public bool actionable = false;
+    public Color haircolor = Color.white;
+    public Color clothescolor = Color.black;
+    public bool isman = true;
+    public string name = "√÷¡æ»£";
+    public List<Direct> directable = new List<Direct>();
+    public Room inroom;
+
+    public GameObject GetWorkerUnit()
+    {
+        GameObject obj = GameObject.Find("Company").transform.Find(name).gameObject;
+        if (obj == null)
+        {
+            obj = new GameObject(name, typeof(WorkerUnit));
+            obj.GetComponent<WorkerUnit>().worker = this;
+            obj.transform.SetParent(GameObject.Find("Company").transform);
+        }
+        return obj;
+    }
+    public enum Room
+    {
+        Art,
+        Program,
+        Direct,
+        Rest
+    }
+
+    [Serializable]
+    public class Direct
+    {
+        public Genre genre = Genre.FPS;
+        public Rank rank = Rank.D;
+
+        public enum Genre
+        {
+            FPS,
+            RTS,
+            Platformer,
+            Loguelike,
+            Action,
+            Metrovania,
+            Simulation,
+            Stealthaction,
+            Hackandslash,
+            Tycoon,
+            Shooting,
+            Rhythm,
+            Curtainfire,
+            Survival,
+            Openworld,
+            Reasoning,
+            Visualnovel,
+            Girldatingsimulation,
+            Turnbasedstrategy,
+            AOS,
+            Autobattler,
+            Sandbox,
+            RPG,
+            MMORPG,
+            Puzzle,
+            Casual,
+            Card,
+            Adventure
+        }
+    }
+}
+
 
 public class WorkerUnit : MonoBehaviour
 {
@@ -18,5 +104,8 @@ public class WorkerUnit : MonoBehaviour
     void Update()
     {
         
+
     }
+
+
 }
