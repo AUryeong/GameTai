de5ecff6970e03c9a7cab0f8258718ee85c467f3
AUryeong <jykee7030@gmail.com>
2022-03-22 17:19:07 +0900

    인게임 대강

---
+++ Assets/Scripts/InGameScene/Worker/WorkerUnit.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkerUnit : MonoBehaviour
+{
+    public Worker worker;
+    public WorkerUnit(Worker worker)
+    {
+        this.worker = worker;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
